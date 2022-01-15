f551a3e2339052781d42faebbc529c07a3a79892
Quentin Lefèvre <qlefevre@gmail.com>
2022-01-15 22:58:20 +0100

    Update json-stability-warrants.py, pipeline.bat, and update-page.py

--- json-stability-warrants.py
+++ json-stability-warrants.py
@@ -5,10 +5,10 @@ with open('output.csv', 'r') as file_csv:
 	fieldnames = ['mnémo','isin','sous-jacent','borne basse','borne haute','maturité','maturité jours','achat','vente','prix sous-jacent','plage','cible','ecart cible','ecart cible abs']
 	reader = csv.DictReader(file_csv, fieldnames, delimiter=';')
 
-	with open('output.json', 'w') as file_json:
+	with open('output.json', 'w', encoding='utf8') as file_json:
 		file_json.write('[')
 		for index, row in enumerate(reader):
 			if(index!=0):
 				file_json.write(',')
-			json.dump(row, file_json)
+			json.dump(row, file_json,ensure_ascii=False)
 		file_json.write(']')
---
+++ pipeline.bat
@@ -0,0 +1,4 @@
+python %CD%/sort-stability-warrants.py
+python %CD%/json-stability-warrants.py
+python %CD%/update-page.py
+pause
