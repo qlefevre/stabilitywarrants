3a429deef296d96bfe4f300ccbb6cab62148afb7
Quentin Lefèvre <qlefevre@gmail.com>
2022-01-21 17:01:35 +0100

    Update extract-sg.py, import.csv, and output.csv

--- extract-sg.py
+++ extract-sg.py
@@ -1,17 +1,26 @@
 import urllib.request
 import utils
+import tempfile
 
-"""
-Download stability warrants from Société Générale 
-"""
-url = 'https://bourse.societegenerale.fr/EmcWebApi/api/ProductSearch/Export?PageNum=1&ProductClassificationId=8'
 
-urllib.request.urlretrieve(url, 'stabilitywarrants-sg.xlsx')
-with open('stabilitywarrants-sg.csv', 'w') as file_csv:
-    rows = utils.xlsx('stabilitywarrants-sg.xlsx');
-    for row in rows:
-        file_csv.write(
-            row['A'] + ';' + row['B'] + ';' + row['C'] + ';' + row['D'] + ';' + row['E'] + ';' + row['F'] + ';' + row[
-                'G'] + ';' + row['H'] + ';' + row['I'] + ';' + row['J'] + '\n')
-utils.upload_file('stabilitywarrants-sg.xlsx', 'raw/sg/xlsx/%Y/%m/stabilitywarrants-sg-%Y-%m-%d.xslx')
-utils.upload_file('stabilitywarrants-sg.csv', 'raw/sg/csv/%Y/%m/stabilitywarrants-sg-%Y-%m-%d.csv')
+def handle(event, context):
+    """
+    Download stability warrants from Société Générale
+    """
+    url = 'https://bourse.societegenerale.fr/EmcWebApi/api/ProductSearch/Export?PageNum=1&ProductClassificationId=8'
+
+    stabilitywarrants_sg_xlsx, headers = urllib.request.urlretrieve(url)
+    stabilitywarrants_sg_csv = tempfile.NamedTemporaryFile(delete=False)
+    stabilitywarrants_sg_csv.close()
+    with open(stabilitywarrants_sg_csv.name, 'w') as csv_file:
+        rows = utils.xlsx(stabilitywarrants_sg_xlsx)
+        for row in rows:
+            csv_file.write(
+                row['A'] + ';' + row['B'] + ';' + row['C'] + ';' + row['D'] + ';' + row['E'] + ';' + row['F'] + ';' +
+                row['G'] + ';' + row['H'] + ';' + row['I'] + ';' + row['J'] + '\n')
+    utils.upload_file(stabilitywarrants_sg_xlsx, 'raw/sg/xlsx/%Y/%m/stabilitywarrants-sg-%Y-%m-%d.xslx')
+    utils.upload_file(stabilitywarrants_sg_csv.name, 'raw/sg/csv/%Y/%m/stabilitywarrants-sg-%Y-%m-%d.csv')
+
+
+if __name__ == '__main__':
+    handle(None, None)
