73fd73e992cad481ad59ffdf3fcfee450ff70cee
Quentin Lefèvre <qlefevre@gmail.com>
2022-01-30 21:46:47 +0100

    csv to json index.html

--- src/csv-to-json.py
+++ src/csv-to-json.py
@@ -10,17 +10,16 @@ def handle(event, context):
     stabilitywarrants_csv = utils.download_file('csv/%Y/%m/stabilitywarrants-%Y-%m-%d.csv')
     stabilitywarrants_json = utils.createTempFile()
     with open(stabilitywarrants_csv, 'r') as file_csv:
-        fieldnames = ['mnémo', 'isin', 'sous-jacent', 'borne basse', 'borne haute', 'maturité', 'maturité jours',
-                      'achat',
-                      'vente', 'prix sous-jacent', 'plage', 'cible', 'ecart cible', 'ecart cible abs']
+        fieldnames = ['isin','sous-jacent','borne basse','borne haute','maturite','maturite jours','achat','vente','prix sous-jacent','plage','cible','ecart cible','ecart cible abs']
         reader = csv.DictReader(file_csv, fieldnames, delimiter=';')
 
         with open(stabilitywarrants_json, 'w', encoding='utf8') as file_json:
             file_json.write('[')
             for index, row in enumerate(reader):
-                if (index != 0):
-                    file_json.write(',')
-                json.dump(row, file_json, ensure_ascii=False, separators=(',', ':'))
+                if index > 0:
+                    if index > 1:
+                        file_json.write(',')
+                    json.dump(row, file_json, ensure_ascii=False, separators=(',', ':'))
             file_json.write(']')
     utils.upload_file(stabilitywarrants_json, 'json/%Y/%m/stabilitywarrants-%Y-%m-%d.json')
 
