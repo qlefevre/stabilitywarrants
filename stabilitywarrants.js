110f52b02604a906bf1ef03fcb83e3179ec0518c
Quentin Lefèvre <qlefevre@gmail.com>
2022-02-07 22:25:48 +0100

    Revue filtrage sous-jacent + maturité jours + perfs

--- stabilitywarrants.js
+++ stabilitywarrants.js
@@ -23,31 +23,37 @@ new Vue({
         { text: 'Borne basse', value: 'borne basse' },
         { text: 'Borne haute', value: 'borne haute' },
         { text: 'Maturité', value: 'maturite' },
-        {
-          text: 'Maturité jours', value: 'maturite jours',
-          filter: value => {
-            if (!this.filtermaturitydays) return true
-            filterval = parseInt(this.filtermaturitydays)
-            return value < filterval & value > (filterval - 30)
-          }
-        },
+        { text: 'Maturité jours', value: 'maturite jours' },
         { text: 'Plage', value: 'plage' },
         { text: 'Cible', value: 'cible' },
         { text: 'Ecart cible abs', value: 'ecart cible abs' },
-        { text: 'Perf min %', value: 'perf min'},
-        { text: 'Perf max %', value: 'perf max'}
+        { text: 'Perf min %', value: 'perf min' },
+        { text: 'Perf max %', value: 'perf max' }
       ],
       warrants: [],
     }
   },
   computed: {
     filteredWarrants() {
-      return !this.filterperf ? this.warrants :
-      this.warrants.filter(warrant => warrant['perf max'] > 8 && warrant['perf min'] > 15 && warrant['perf min'] > warrant['perf max']);
+      var warrants0 = this.warrants;
+      if (this.filterperf) {
+        warrants0 = warrants0.filter(warrant =>
+          warrant['perf max'] > 8 &&
+          warrant['perf min'] > 15 &&
+          Fwarrant['perf min'] > warrant['perf max']);
+      }
+      if (this.filtermaturitydays != '') {
+        var filterval = parseInt(this.filtermaturitydays)
+        warrants0 = warrants0.filter(warrant =>
+          warrant['maturite jours'] < filterval &
+          warrant['maturite jours'] > (filterval - 30));
+      }
+      return warrants0;
     },
     sousjacents() {
       var sousjacents0 = this.filteredWarrants.map(warrant => warrant['sous-jacent']);
       sousjacents0 = sousjacents0.filter((x, i, a) => a.indexOf(x) == i);
+      sousjacents0.unshift('');
       return sousjacents0;
     }
   },
