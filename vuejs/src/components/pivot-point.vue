6639c4c293bffbaafe84c77fb5af97eac35bdfd3
Quentin Lefèvre <qlefevre@gmail.com>
2022-04-07 12:10:55 +0200

    Modification affichage du composant point pivot

--- vuejs/src/components/pivot-point.vue
+++ vuejs/src/components/pivot-point.vue
@@ -1,13 +1,20 @@
 <template>
   <div class="pivotpoint">
-    {{ this.value.period }}
-    <span class="support">S3:</span> {{ this.value.pivotpoint.S3 }}
-    <span class="support">S2:</span> {{ this.value.pivotpoint.S2 }}
-    <span class="support">S1:</span> {{ this.value.pivotpoint.S1 }}
-    <span class="pivot">Pivot:</span> {{ this.value.pivotpoint.P }}
-    <span class="resistance">R1:</span> {{ this.value.pivotpoint.R1 }}
-    <span class="resistance">R2:</span> {{ this.value.pivotpoint.R2 }}
-    <span class="resistance">R3:</span> {{ this.value.pivotpoint.R3 }}
+    <div class="libelle">{{ this.value.period }}</div>
+    <div class="support">S3:</div>
+    {{ this.value.pivotpoint.S3 }}
+    <div class="support">S2:</div>
+    {{ this.value.pivotpoint.S2 }}
+    <div class="support">S1:</div>
+    {{ this.value.pivotpoint.S1 }}
+    <div class="pivot">Pivot:</div>
+    {{ this.value.pivotpoint.P }}
+    <div class="resistance">R1:</div>
+    {{ this.value.pivotpoint.R1 }}
+    <div class="resistance">R2:</div>
+    {{ this.value.pivotpoint.R2 }}
+    <div class="resistance">R3:</div>
+    {{ this.value.pivotpoint.R3 }}
   </div>
 </template>
 
@@ -27,11 +34,17 @@ module.exports = {
 .resistance {
   color: green;
 }
-.pivotpoint span {
-  padding: 0px 5px 0px 20px;
+.pivotpoint div.libelle {
+  text-align: right;
+  width: 90px;
+}
+.pivotpoint div {
+  padding: 0px 5px 0px 12px;
+  display: inline-block;
 }
 .pivotpoint {
   margin: 0 auto;
   width: auto;
+  margin-bottom: 5px;
 }
 </style>
