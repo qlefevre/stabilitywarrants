7dedfaf940639863d83531f5f0ccc4562b3505b6
Quentin Lefèvre <qlefevre@gmail.com>
2022-04-15 00:31:10 +0200

    Modification du mode de déclenchement (trop d'erreurs de lancement côté Scaleway) passage à une seule function avec la chaine

--- functions/run_all_functions.py
+++ functions/run_all_functions.py
@@ -4,9 +4,12 @@ import merge
 import transform
 import csv_to_json
 
-if __name__ == '__main__':
+def handle(event, context):
     extract_sg.handle(None, None)
     extract_uc.handle(None, None)
     merge.handle(None, None)
     transform.handle(None, None)
     csv_to_json.handle(None, None)
+
+if __name__ == '__main__':
+    handle(None, None)
