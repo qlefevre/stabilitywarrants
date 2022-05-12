0cf264e94bce0789be5700ae225fb238c7952b18
Quentin Lefèvre <qlefevre@gmail.com>
2022-05-12 22:01:46 +0200

    Pb sur le mixin

---
+++ vuejs/src/components/table-mixin.js
@@ -0,0 +1,14 @@
+// define a mixin object
+tableMixin = {
+    methods: {
+        getStyle(perf) {
+            if (perf < 7) return "red-value";
+            else if (perf < 12) return "orange-value";
+            else return "";
+        },
+        getPvStyle(pv) {
+            if (pv < 0) return "red-pv";
+            return "green-pv";
+        }
+    }
+};
